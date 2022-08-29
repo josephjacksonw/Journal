c4c5039c0babbdec5dee5107c95dfd77e9781041
josephjacksonw <josephjacksonw@gmail.com>
2022-08-29 18:02:52 -0400

    Moved buisiness logic to entry.js
    Co-authored-by: Athea DeLing <almdeling@gmail.com>

--- src/entry.js
+++ src/entry.js
@@ -1,14 +1,52 @@
 export default function Entry(title, passage) {
   this.title = title;
   this.passage = passage;
-}
+ }
 
-Entry.prototype.wordAnalyzer = function(e) {
+Entry.prototype.wordAnalyzer = function() {
   let words = 0;
-  let passArr = []
-  passArr = e.passage.split(" ");
+  let passArr = [];
+  passArr = this.passage.split(" ");
+  if (this.passage === "") {
+    return 0;
+  }
   passArr.forEach(function(){
     words += 1;
   });
   return words;
 };
+
+Entry.prototype.sampleSentence = function() {
+  let passArr = [];
+  passArr = this.passage.split(" ");
+  let sentenceArr = [];
+  for(let i = 0; i < 8; i++) {
+    sentenceArr.push(passArr[i])
+    if ( i === passArr.length || passArr[i].includes(".")) {
+      i = 9;
+    }
+  }
+  return sentenceArr;
+}
+
+Entry.prototype.vCounter = function() {
+  let vowels = 0
+  let passArr = this.passage.split("")
+  passArr.forEach(function (i) {
+    if (i === "a" || i === "e" || i === "o" || i === "i" || i === "u" || i === "y") {
+      vowels += 1;
+    }
+  });
+  return vowels;
+}
+
+Entry.prototype.cCounter = function() {
+  let consonant = 0
+  let passArr = this.passage.split("")
+  passArr.forEach(function (i) {
+    if (i != "a" || i != "e" || i != "o" || i != "i" || i != "u" || i != "y") {
+      consonant += 1;
+    }
+  });
+  return consonant;
+}
--- src/index.js
+++ src/index.js
@@ -2,44 +2,26 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Entry from './entry.js';
-//import wordAnalyzer from './entry.js';
-function handleTriangleForm(event) {
+
+function handleJournal(event) {
   event.preventDefault();
   const title = document.getElementById("title").value;
   const passage = document.getElementById("passage").value;
-  console.log(title + " " + typeof(title));
-  console.log(passage);
-  let entry = new Entry(title, passage);
-  console.log(entry);
-  let words = 0
-  let passArr = passage.split(" ")
-  passArr.forEach(function(i) {
-    console.log(i)
-    words += 1
-  });
-  passArr = passage.split("")
+  const entry = new Entry(title, passage);
+  const words = entry.wordAnalyzer()
+  const sentenceArr = entry.sampleSentence()
   let vowels = 0
   let consonants = 0 
-  passArr.forEach(function(i) {
-    if (i === "a" || i === "e" || i === "o" || i === "i" || i === "u" || i === "y") {
-      vowels += 1;
-    } else {
-      consonants += 1;
-    }
-  })
-  console.log(passArr)
+  vowels = entry.vCounter()
+  consonants = entry.cCounter()  
+  let firstSentence = document.getElementById("first-sentence")
+  firstSentence.innerText = sentenceArr.join(" ")
   let tws = document.getElementById("tws");
   tws.innerText = words;
   let vs = document.getElementById("vs");
   vs.innerText = vowels;
   let cs = document.getElementById("cs");
   cs.innerText = consonants;
-  
-  //words = wordAnalyzer(entry);
-  //console.log(words);
 }
 
-
-
-document.querySelector("form#journal-entry").addEventListener("submit", handleTriangleForm);
-
+document.querySelector("form#journal-entry").addEventListener("submit", handleJournal);
